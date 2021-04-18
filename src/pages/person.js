60597d1773115417db5bd20f5a08733076884518
Kaustubh Verma <vermakaustubh28@gmail.com>
2021-04-18 19:28:18 +0530

    completed profile part

--- src/pages/person.js
+++ src/pages/person.js
@@ -4,6 +4,9 @@ import imageLogo from '../content/images/Logo.png'
 import imagePerson from "../content/images/person.jpg"
 import styling from '../content/styling/styles.css'
 import Year from "./year"
+import SettingsIcon from '@material-ui/icons/Settings';
+import { LoremIpsum } from 'react-lorem-ipsum';
+import ContactsIcon from '@material-ui/icons/Contacts';
 // @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap');
 
 
@@ -11,7 +14,7 @@ const ISTEgram = styled.div`
   ${'' /* text-align:center; */}
   border:2px solid black;
   padding:1%;
-  margin: 0px 15%;
+  margin: 0px 23%;
   img{
     display: inline-flex;
     margin:0;
@@ -30,32 +33,62 @@ const ISTEgram = styled.div`
   .profile{
     border:3px solid black;
     margin:2%;
-    img{
+    display:flex;
+    .details{
+      width:35%;
+      img{
       margin:1%;
       border-radius:100%;
-      ${'' /* display: inline-flex; */}
+      }
+      h3{
+        text-align:center;
+        ${'' /* width:fit-content; */}
+        margin:1%;
+      }  
     }
-    div{
-      ${'' /* position:absolute; */}
+    .bio{
       width:65%;
       float:right;
-      top:0;
+      margin-left:4% ;
       border:1px solid black;
       table{
-        float:right;
-        margin-right:5%;
-        ${'' /* position:relative; */}
+        border:none;
+        ${'' /* float:right; */}
+        ${'' /* align-items:center; */}
+        margin-left:10%;
+        margin-bottom:0;
         width:80%;
         th{
+          border:none;
           text-align:center;
           font-size:2vw;
+          padding:0;
         }
         td{
+          border:none;
           text-align:center;
-          font-size:1.5vw;
+          font-size:1vw;
+          padding:0;
+        }
+      }
+      h3{
+        margin:1% 0 1% 4%;
+        ${'' /* margin-top:1%; */}
+        ${'' /* text-align:center; */}
+        .span1{
+          padding:1% 24%;
+          border:1px solid black;
+          margin-right:2%;
+          border-radius:5px;
+        }
+        .span2{
+          padding:1% 3%;
+          border-radius:5px;
+          border:1px solid black;
         }
       }
     }
+
 
   }
 `;
@@ -70,9 +103,17 @@ export default function person() {
             <h1><span>ISTE</span>gram</h1>
           </div>
           <div className="profile">
-            <img src={imagePerson} alt="My Image" width="30%"/>
-            <div>
+            <div className="details">
+              <img src={imagePerson} alt="My Image" width="100%"/>
+              <h3>Kaustubh Verma<span style={{margin:"1%"}}><ContactsIcon style={{fill:"blue"}}/></span></h3>
+              {/* <p>Hello world</p> */}
+            </div>
+            <div className="bio">
               <Year />
+              <div className="text-wrapper">
+                <h3><span className="span1">Profile</span><span className="span2"><SettingsIcon /></span></h3>
+                <LoremIpsum p={1} />
+              </div>
             </div>
           </div>
         </div>
--- src/pages/year.js
+++ src/pages/year.js
@@ -1,26 +1,4 @@
 import React from "react"
-import styled from 'styled-components'
-import imageLogo from '../content/images/Logo.png'
-import imagePerson from "../content/images/person.jpg"
-import styling from '../content/styling/styles.css'
-
-const tableStyle = styled.div`
-  table{
-        float:right;
-        margin-right:5%;
-        ${'' /* position:relative; */}
-        width:80%;
-        th{
-          text-align:center;
-          font-size:2vw;
-        }
-        td{
-          text-align:center;
-          font-size:1.5vw;
-        }
-  }
-`;
-
 
 
 export default function Year(){
@@ -29,7 +7,7 @@ export default function Year(){
       <tableStyle>
         <table>
         <tr>
-          <th>29</th>
+          <th>28</th>
           <th>10</th>
           <th>2003</th>
         </tr>
